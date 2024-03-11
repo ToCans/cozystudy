c2cd6a16f32ef57e932864ab2bcacf572595e236
ToCans <Thomas.Cansfield@gmail.com>
2024-03-11 13:06:12 +0100

    Improved Audio Resource Calls. Less calls made

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,6 +1,4 @@
 import "../index.css";
-//import completeSound from "https://github.com/ToCans/cozystudy/blob/main/src/assets/complete.mp3?raw=true";
-//import lowHighChimeSound from "https://github.com/ToCans/cozystudy/blob/main/src/assets/lowHighChime.mp3?raw=true";
 import TimeDisplay from "./timeDisplay";
 import InteractiveButton from "./timeButton";
 import ProgressBarTest from "./progressBar";
@@ -23,14 +21,6 @@ function Timer() {
     const [progressBarValue, setProgressBarValue] = useState(0);
     const [progressBarTotal, setProgressBarTotal] = useState(
         settings.workingMinutes * 60 + settings.workingSeconds
-    );
-
-    // Audio Used
-    const workFinishAudio = new Audio(
-        "https://github.com/ToCans/cozystudy/blob/main/src/assets/sounds/complete.mp3?raw=true"
-    );
-    const breakFinishAudio = new Audio(
-        "https://github.com/ToCans/cozystudy/blob/main/src/assets/sounds/lowHighChime.mp3?raw=true"
     );
 
     // Used to Decrement the time
@@ -110,16 +100,29 @@ function Timer() {
         ) {
             // lowHighChime played for breaks finishing
             if (settings.cycleNumber % 2 === 0) {
+                // Audio Used
+
+                const breakFinishAudio = new Audio(
+                    "https://github.com/ToCans/cozystudy/blob/main/src/assets/sounds/lowHighChime.mp3?raw=true"
+                );
                 breakFinishAudio.volume = 0.5;
                 breakFinishAudio.play();
             }
             // Complete played for work finishing
             else {
+                const workFinishAudio = new Audio(
+                    "https://github.com/ToCans/cozystudy/blob/main/src/assets/sounds/complete.mp3?raw=true"
+                );
                 workFinishAudio.volume = 0.5;
                 workFinishAudio.play();
             }
         }
-    });
+    }, [
+        settings.cycleNumber,
+        minutesRemaining,
+        secondsRemaining,
+        timerRunning,
+    ]);
 
     // Timer Display Information
     let cycleDisplay = Math.ceil(settings.cycleNumber / 2);
