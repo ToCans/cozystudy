f6aaca53311112c963f04ccd3cbabd2544df3665
ToCans <Thomas.Cansfield@gmail.com>
2024-06-10 08:20:52 +0200

    Update Image Icon

--- src/components/photoCredit.jsx
+++ src/components/photoCredit.jsx
@@ -2,7 +2,10 @@ const PhotoCredit = ({ photographer, portfolio }) => {
     return (
         <div className="flex flex-row w-full h-fit bottom-0 absolute">
             <span className="grow"></span>
-            <div className="text-slate-50 bg-slate-400 rounded-t-lg mx-2 p-2 opacity-95">
+            <div
+                className="text-slate-50 bg-slate-400 rounded-t-lg mx-10 sm:mx-4
+             p-2 opacity-95"
+            >
                 <a
                     href={portfolio}
                     target="#blank"
